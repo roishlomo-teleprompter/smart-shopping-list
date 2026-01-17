9589cfd08788738bb7a3f31c787e8ea67566217a
Roi Shlomo <roi.shlomo@gmail.com>
2026-01-17 13:12:47 +0200

    Fix Firebase config and API key

--- firebase.ts
+++ firebase.ts
@@ -3,15 +3,17 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: "AIzaSyCEUaqAGQBy_-tQEjnkNGYMXRk7YRaCAjI",
+  authDomain: "shopping-list-218bd.firebaseapp.com",
+  projectId: "shopping-list-218bd",
+  storageBucket: "shopping-list-218bd.firebasestorage.app",
+  messagingSenderId: "883804592996",
+  appId: "1:883804592996:web:61b0bb28cd02ef3961b871",
 };
 
 export const app = initializeApp(firebaseConfig);
+
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
+
 export const db = getFirestore(app);
